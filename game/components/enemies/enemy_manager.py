10fdde19a878b56685055dd45816f6ff8f002a67
JoelSoliz <joelsolizchoque@gmail.com>
2023-06-15 15:56:14 -0400

    Class 3: Bullets

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -6,13 +6,13 @@ class EnemyManager:
     def __init__(self):
         self.enemies: list[Enemy] = []
 
-    def update(self):
+    def update(self, game):
         if not self.enemies: # [] {} 0 "" -> false | [1] {1: 1} 1 -2 "asd" -> true
             enemy_variant = random.randint(1, 2)
             self.enemies.append(Enemy(enemy_variant))
 
         for enemy in self.enemies:
-            enemy.update(self.enemies)
+            enemy.update(self.enemies, game)
 
     def draw(self, screen):
         for enemy in self.enemies:
